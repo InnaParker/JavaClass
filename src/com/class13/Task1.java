84060714e54a30ad6692d4f3e8531e344b54d569
InnaParker <inna.travels1@gmail.com>
2019-03-28 21:56:42 -0400

    Class Tasks

---
+++ src/com/class13/Task1.java
@@ -0,0 +1,19 @@
+package com.class13;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+		// Create a string and print it in reverse order - Sunday.
+		
+		String str="Sunday";
+		
+		for (int i=str.length()-1; i>=0; i--) {			
+			System.out.print(str.charAt(i));
+
+			
+		}
+		
+	}
+	
+}
